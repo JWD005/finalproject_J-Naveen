edb126684ef1b9802049f31d440cee97d0b51ef4
Yiteng Huang <hyy528924543@gmail.com>
2021-06-12 00:49:29 +1000

    form validation check finish

--- js/columnControl.js
+++ js/columnControl.js
@@ -1,85 +1,3 @@
-// Task 5 initialize New Task Manager- code to be console.log()
-// const taskManager = new TaskManager();//to be set
-/*
-Function validFormFieldInput(data) 
-
-const form = document.querySelector("#new-task-form");
-
-const taskName = document.querySelector('#taskName');
-const taskDescription = document.querySelector('#description');
-const dueDate = document.querySelector('#dueDate');
-const personAssigned = document.querySelector('#assignedPPL');
-const statusField = document.querySelector('#status');
-const btn = document.querySelector('#btn');
-
-
-form.addEventListener("submit", (event) => {
-    const validateName = document.querySelector("#taskName");
-    const validateDescription = document.querySelector("#description");
-    const validateAssignedTo = document.querySelector("#dueDate");
-    const validateDueDate = document.querySelector("#assignedPPL");
-    const validateStatus = document.querySelector("#status");
-    let validationFail = 0;
-
-    event.preventDefault();
-    event.stopPropagation();
-    console.log("Task Name :" + validateName.value.length);
-    console.log("Task Description :" + validateDescription.value);
-    console.log("Task Assigned To :" + validateAssignedTo.value);
-    console.log("Task Due Date :" + validateDueDate.value);
-    console.log("Task Status:" + validateStatus.value);
-
-    //Form validation for taskName
-if (validateName.value) {
-    validateName.classList.add("is-valid");
-    validateName.classList.remove("is-invalid");
-} else {
-    validateName.classList.add("is-invalid");
-    validateName.classList.remove("is-valid");
-    validationFail++;
-}
-//Form validation for description
-if (validateDescription.value) {
-    validateDescription.classList.add("is-valid");
-    validateDescription.classList.remove("is-invalid");
-} else {
-    validateDescription.classList.add("is-invalid");
-    validateDescription.classList.remove("is-valid");
-    validationFail++;
-}
-//Form validation for deuDate
-if (validateDueDate.value) {
-    validateDueDate.classList.add("is-valid");
-    validateDueDate.classList.remove("is-invalid");
-} else {
-    validateDueDate.classList.add("is-invalid");
-    validateDueDate.classList.remove("is-valid");
-    validationFail++;
-}
-//Form validation for assignedPPL
-if (validateAssignedTo.value) {
-    validateAssignedTo.classList.add("is-valid");
-    validateAssignedTo.classList.remove("is-invalid");
-} else {
-    validateAssignedTo.classList.add("is-invalid");
-    validateAssignedTo.classList.remove("is-valid");
-    validationFail++;
-}  
-//Form validation for status
-if (validateAssignedTo.value) {
-    validateAssignedTo.classList.add("is-valid");
-    validateAssignedTo.classList.remove("is-invalid");
-} else {
-    validateAssignedTo.classList.add("is-invalid");
-    validateAssignedTo.classList.remove("is-valid");
-    validationFail++;
-}
-if (validationFail > 0) {
-    validationFail = 0;
-    return;
-}
-});
-*/
 const colToStart = document.getElementById("colToStart");
 const colInProgress = document.getElementById("colInProgress");
 const colInReview = document.getElementById("colInReview");
@@ -89,11 +7,11 @@ const colCompleted = document.getElementById("colCompleted");
 var winWidth = $(window).width();
 $(window).resize(function () {
   winWidth = $(window).width();
-  console.log(winWidth);
+  toggleToStartColOpen();
 });
 
 function toggleToStartColOpen() {
-  if (winWidth > 1500) {
+  if (winWidth > 1200) {
     colToStart.style.width = "Calc(100% - 22rem)";
     colToStart.style.height = "85vh";
     colToStart.style.backgroundColor = "rgb(255, 229, 209)";
@@ -106,7 +24,7 @@ function toggleToStartColOpen() {
   document.querySelector("#colToStart > div").style.display = "flex";
 }
 function toggleToStartColOff() {
-  if (winWidth > 1500) {
+  if (winWidth > 1200) {
     colToStart.style.width = "8rem";
     colToStart.style.height = "85vh";
     colToStart.style.backgroundColor = "rgb(255, 239, 227)";
