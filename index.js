000724a43378cd024090cf31abbef2cc180ceed9
JJ <jc@orbsdigital.com>
2021-06-11 13:13:30 +1000

    event listener

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-function validFormFieldInput(data);
+Function validFormFieldInput(data) {}
 
-validFormFieldInput.addEventListener("submit", (event) => {
-    const taskName = document.querySelector('#taskName');
-    const taskDescription = document.querySelector('#taskDescription');
-    const dueDate = document.querySelector('#dueDate');
-    const personAssigned = document.querySelector('#personAssigned');
-    const statusField = document.querySelector('#statusField');
-    const btn = document.querySelector('#btn');
+const taskName = document.querySelector('#taskName');
+const taskDescription = document.querySelector('#description');
+const dueDate = document.querySelector('#dueDate');
+const personAssigned = document.querySelector('#assignedPPL');
+const statusField = document.querySelector('#status');
+const btn = document.querySelector('#btn');
+
+
+form.addEventListener("submit", (event) => {
+    const validateName = document.querySelector("#taskName");
+    const validateDescription = document.querySelector("#description");
+    const validateAssignedTo = document.querySelector("#dueDate");
+    const validateDueDate = document.querySelector("#assignedPPL");
+    const validateStatus = document.querySelector("#status");
+    let validationFail = 0;
 }
 
+//Form validation for taskName
+
+//Form validation for taskDescription
+//Form validation for deuDate
+//Form validation for personAssigned
+//Form validation for statusField
