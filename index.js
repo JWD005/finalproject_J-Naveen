de5fe1f79436ec25f3bcb4caf4d1ef6bdc815809
JJ <jc@orbsdigital.com>
2021-06-11 11:25:16 +1000

    adding ID attribute to form

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-//just some content
-validFormFieldInput(data){
+function validFormFieldInput(data);
 
+validFormFieldInput.addEventListener("submit", (event) => {
+    const taskName = document.querySelector('#taskName');
+    const taskDescription = document.querySelector('#taskDescription');
+    const dueDate = document.querySelector('#dueDate');
+    const personAssigned = document.querySelector('#personAssigned');
+    const statusField = document.querySelector('#statusField');
+    const btn = document.querySelector('#btn');
 }
+
