23069ef35ad482224d9a1d7bef41a8c07431ea5e
JJ <jc@orbsdigital.com>
2021-06-12 16:58:41 +1000

    completing task 6 steps

--- js/taskManager.js
+++ js/taskManager.js
@@ -1,3 +1,4 @@
+/*
 // initialize or create four arrays to store objects(cards) under different status
 // to store data into Local Storage, use
 // localStorage.setItem("nameOfItem", JSON.stringify(whatShouldBeStored))
@@ -18,16 +19,46 @@ const inReviewArr = JSON.parse(localStorage.getItem("inReviewArr"))
 const completedArr = JSON.parse(localStorage.getItem("completedArr"))
   ? JSON.parse(localStorage.getItem("completedArr"))
   : [];
-
-/* 
-// Task 5: initialize taskManager class
-class taskManager {
-  constructor() { // parameters to be set
-    this.task = [];// initialize this. to an empty array
-    }
 */
 
+/*
+Task 5: initialize taskManager class
+Step 2: The TaskManager Class
+In this step, we'll create a TaskManager class that will be responsible for managing the tasks in the application.
+Create a TaskManager class in js/taskManager.js
+Within the constructor of the TaskManager class, initialize a this.tasks property on the class equal to an empty array.
 
+Task 6: Adding A New Task Programmatically
+Step 1: 
+1.In the TaskManager's constructor, accept a currentId parameter, with a default value of 0.
+2.Assign the currentId to a new property on the class, this.currentId.
+3.Create a method on the class, addTask. 
+4.Within the addTask method, increment the this.currentId
+5.push a new task into the this.tasks array, with the correct properties of the task, using the values passed in as parameters as well as the new this.currentId
+*/
+
+class taskManager { //<--Task 5- Create a TaskManager class in js/taskManager.js
+  constructor(currentId = 0) { // <--Task 6- 1.In the TaskManager's constructor, accept a currentId parameter, with a default value of 0.
+    this.task = []; // <-- Task 5- Within the constructor of the TaskManager class, initialize a this.tasks property on the class equal to an empty array.
+    this.currentId = currentId; // <--Task 6- 2.Assign the currentId to a new property on the class, this.currentId.
+  }
+  addTask(taskName, decription, assignedTo, dueDate, status){ //<--Task 6- Create a method on the class, addTask. 
+    const task = {
+      id: this.currentId++, //<--Task 6- Within the addTask method, increment the this.currentId.
+      taskName: taskName,
+      description: description,
+      assignedTo: assignedTo,
+      dueDate: dueDate,
+      status: status,
+    };
+    this.task.push({task});
+  } 
+
+
+
+
+
+/*
 // initialize taskManager class
 // as the id is highly associated with the status of each card
 // means when a card move from one status into another, the id should be changed based on the new status array.
@@ -79,3 +110,4 @@ class taskManager {
     this._id = newId;
   }
 }
+*/
